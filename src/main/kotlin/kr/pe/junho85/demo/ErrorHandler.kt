8343ace136dd77217d1c3d920248f54d8b333987
june.kim <junho85@gmail.com>
2020-07-06 15:32:42 +0900

    ~p168

---
+++ src/main/kotlin/kr/pe/junho85/demo/ErrorHandler.kt
@@ -0,0 +1,24 @@
+package kr.pe.junho85.demo
+
+import com.fasterxml.jackson.core.JsonParseException
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
+import org.springframework.web.bind.annotation.ControllerAdvice
+import org.springframework.web.bind.annotation.ExceptionHandler
+import javax.servlet.http.HttpServletRequest
+
+//@ControllerAdvice
+//class ErrorHandler {
+//    @ExceptionHandler(JsonParseException::class)
+//    fun JsonParseExceptionHandler(servletRequest: HttpServletRequest, exception: Exception): ResponseEntity<String> {
+//        return ResponseEntity("JSON Error", HttpStatus.BAD_REQUEST)
+//    }
+//}
+
+@ControllerAdvice
+class ErrorHandler {
+    @ExceptionHandler(JsonParseException::class)
+    fun JsonParseExceptionHandler(servletRequest: HttpServletRequest, exception: Exception): ResponseEntity<ErrorResponse> {
+        return ResponseEntity(ErrorResponse("JSON Error", exception.message ?: "invalid json"), HttpStatus.BAD_REQUEST)
+    }
+}
---
+++ src/main/kotlin/kr/pe/junho85/demo/ErrorResponse.kt
@@ -0,0 +1,3 @@
+package kr.pe.junho85.demo
+
+data class ErrorResponse(val error: String, val message: String)
