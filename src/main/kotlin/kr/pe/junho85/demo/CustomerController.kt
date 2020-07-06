1f58942e6492c2bbe6701b9d0a80dad2fdeff2de
june.kim <junho85@gmail.com>
2020-07-06 14:51:59 +0900

    ~p164

--- src/main/kotlin/kr/pe/junho85/demo/CustomerController.kt
+++ src/main/kotlin/kr/pe/junho85/demo/CustomerController.kt
@@ -1,6 +1,8 @@
 package kr.pe.junho85.demo
 
 import org.springframework.beans.factory.annotation.Autowired
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.*
 
 @RestController
@@ -9,19 +11,36 @@ class CustomerController {
     lateinit var customerService: CustomerService
 
     @GetMapping("/customers/{id}")
-    fun getCustomer(@PathVariable id : Int) = customerService.getCustomer(id)
+    fun getCustomer(@PathVariable id : Int): ResponseEntity<Customer> {
+        val customer = customerService.getCustomer(id)
+        val status = if (customer == null) HttpStatus.NOT_FOUND else HttpStatus.OK
+        return ResponseEntity(customer, status)
+    }
 
     @PostMapping("/customers")
-    fun createCustomer(@RequestBody customer: Customer) {
+    fun createCustomer(@RequestBody customer: Customer): ResponseEntity<Unit?> {
         customerService.createCustomer(customer)
+        return ResponseEntity(Unit, HttpStatus.CREATED)
     }
 
     @DeleteMapping("/customers/{id}")
-    fun deleteCustomer(@PathVariable id: Int) = customerService.deleteCustomer(id)
+    fun deleteCustomer(@PathVariable id: Int): ResponseEntity<Unit> {
+        var status = HttpStatus.NOT_FOUND
+        if (customerService.getCustomer(id) != null) {
+            customerService.deleteCustomer(id)
+            status = HttpStatus.OK
+        }
+        return ResponseEntity(Unit, status)
+    }
 
     @PutMapping("/customers/{id}")
-    fun updateCusomter(@PathVariable id: Int, @RequestBody customer: Customer) {
-        customerService.updateCustomer(id, customer)
+    fun updateCusomter(@PathVariable id: Int, @RequestBody customer: Customer): ResponseEntity<Unit> {
+        var status = HttpStatus.NOT_FOUND
+        if (customerService.getCustomer(id) != null) {
+            customerService.updateCustomer(id, customer)
+            status = HttpStatus.ACCEPTED
+        }
+        return ResponseEntity(Unit, status)
     }
 
     @GetMapping("/customers")
--- src/main/kotlin/kr/pe/junho85/demo/CustomerServiceImpl.kt
+++ src/main/kotlin/kr/pe/junho85/demo/CustomerServiceImpl.kt
@@ -1,14 +1,16 @@
 package kr.pe.junho85.demo
 
+import kr.pe.junho85.demo.Customer.*
 import org.springframework.stereotype.Component
 import java.util.concurrent.ConcurrentHashMap
 
 @Component
 class CustomerServiceImpl : CustomerService {
     companion object {
-        val initialCustomers = arrayOf(Customer(1, "Kotlin"),
+        val initialCustomers = arrayOf(
+                Customer(1, "Kotlin"),
                 Customer(2, "Spring"),
-                Customer(3, "Microservice"))
+                Customer(3, "Microservice", Telephone("+44", "7123456789")))
     }
     val customers = ConcurrentHashMap<Int, Customer>(initialCustomers.associateBy(Customer::id))
 
